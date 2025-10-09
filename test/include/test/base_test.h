0d110247f494af761fc2e14f18bc8c684a80817e
Moritz Linsmaier <moritz.linsmaier@tum.de>
2025-10-09 19:46:47 +0200

    adapt BLOCK_DEVICE in base_test to leanstore partition

--- test/include/test/base_test.h
+++ test/include/test/base_test.h
@@ -18,7 +18,7 @@
 
 namespace fs = std::filesystem;
 
-#define BLOCK_DEVICE "/dev/nvme1n1"
+#define BLOCK_DEVICE "/dev/nvme0n1p3"
 #define CHECK_EXTENT_PAGE_STATE(expected_state, start_pid, pg_cnt)                   \
   ({                                                                                 \
     EXPECT_EQ(this->buffer_->GetPageState(start_pid).LockState(), (expected_state)); \
